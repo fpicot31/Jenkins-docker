c88a7081bf2a0188a1ce83ce48e9ea8c0187d126
fpicot31 <80047098+fpicot31@users.noreply.github.com>
2022-11-18 18:47:12 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ else
    echo "Erreur : le reseau docker jenkins n'existe pas !"
 fi
 
-
+echo "Build Jenkins Ocean en cours ..." 
 cmd=`sudo docker build -t myjenkins-blueocean:$VERSION -f Dockerfile-Jenkins .`
 if [ $? -eq 0 ]
 then
@@ -40,6 +40,7 @@ if [ $? -eq 0 ]
 then
    echo "Jenkins-docker deja demarre"
 else
+   echo "Demarrage Jenkins Docker en cours ..." 
    cmd=`sudo docker run --name jenkins-docker --rm --detach   --privileged --network jenkins --network-alias docker   --env DOCKER_TLS_CERTDIR=/certs   --volume jenkins-docker-certs:/certs/client   --volume jenkins-data:/var/jenkins_home   --publish 3000:3000 --publish 5000:5000 --publish 2376:2376   docker:dind --storage-driver overlay2`
 
    cmd=`sudo docker ps | grep jenkins-docker`
@@ -56,6 +57,7 @@ if [ $? -eq 0 ]
 then
    echo "Jenkins ocean deja demarre"
 else
+   echo "Demarrage Jenkins Ocean en cours ..." 
    cmd=`sudo docker run --name jenkins-blueocean --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   --volume "$HOME":/home   --restart=on-failure   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true"   myjenkins-blueocean:$VERSION`
 
    cmd=`sudo docker ps | grep jenkins-blueocean`

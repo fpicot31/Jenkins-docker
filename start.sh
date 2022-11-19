70150b0fe048998241b6d251c71b714aa63af2ec
fpicot31 <80047098+fpicot31@users.noreply.github.com>
2022-11-19 09:57:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -59,10 +59,8 @@ else
    sudo docker run --name jenkins-blueocean --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   --volume "$HOME":/home   --restart=on-failure   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" myjenkins-blueocean:$VERSION
 
    sudo docker ps | grep jenkins-blueocean
-   if [ $? -eq 0 ]
+   if [ $? -ne 0 ]
    then
-      echo "Jenkins ocean OK"
-   else
       echo "Erreur de demarrage de jenkins ocean !"
    fi
 fi

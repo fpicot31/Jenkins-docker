e462789e1389503ed67e8a3dd31b694c44635291
fpicot31 <80047098+fpicot31@users.noreply.github.com>
2022-11-19 09:43:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,16 +9,14 @@
 # Attention cette version est aussi à mettre à jour dans le fichier Dockerfile-Jenkins 
 VERSION=2.361.4-1
 
-cmd=`sudo docker network ls | grep jenkins`
-
-
+sudo docker network ls | grep jenkins
 if [ $? -ne 0 ]
 then
    echo "creation du docker network jenkins"
    sudo docker network create jenkins
 fi
 
-cmd=`sudo docker network ls | grep jenkins`
+sudo docker network ls | grep jenkins
 if [ $? -eq 0 ]
 then
    echo "docker network jenkins OK"
@@ -27,7 +25,7 @@ else
 fi
 
 echo "Build Jenkins Ocean en cours ..." 
-cmd=`sudo docker build -t myjenkins-blueocean:$VERSION -f Dockerfile-Jenkins .`
+sudo docker build -t myjenkins-blueocean:$VERSION -f Dockerfile-Jenkins .
 if [ $? -eq 0 ]
 then
    echo "Image jenkins-blueocean:$VERSION créée dans la base de registre"
@@ -35,15 +33,15 @@ else
    echo "Erreur lors de la creation image jenkins-blueocean:$VERSION !"
 fi
 
-cmd=`sudo docker ps | grep jenkins-docker`
+sudo docker ps | grep jenkins-docker
 if [ $? -eq 0 ]
 then
    echo "Jenkins-docker deja demarre"
 else
    echo "Demarrage Jenkins Docker en cours ..." 
-   cmd=`sudo docker run --name jenkins-docker --rm --detach   --privileged --network jenkins --network-alias docker   --env DOCKER_TLS_CERTDIR=/certs   --volume jenkins-docker-certs:/certs/client   --volume jenkins-data:/var/jenkins_home   --publish 3000:3000 --publish 5000:5000 --publish 2376:2376   docker:dind --storage-driver overlay2`
+   sudo docker run --name jenkins-docker --rm --detach   --privileged --network jenkins --network-alias docker   --env DOCKER_TLS_CERTDIR=/certs   --volume jenkins-docker-certs:/certs/client   --volume jenkins-data:/var/jenkins_home   --publish 3000:3000 --publish 5000:5000 --publish 2376:2376   docker:dind --storage-driver overlay2
 
-   cmd=`sudo docker ps | grep jenkins-docker`
+   sudo docker ps | grep jenkins-docker
    if [ $? -eq 0 ]
    then
       echo "Jenkins-docker OK"
@@ -52,15 +50,15 @@ else
    fi
 fi
 
-cmd=`sudo docker ps | grep jenkins-blueocean`
+sudo docker ps | grep jenkins-blueocean
 if [ $? -eq 0 ]
 then
-   echo "Jenkins ocean deja demarre"
+   echo "Jenkins Ocean deja demarre"
 else
    echo "Demarrage Jenkins Ocean en cours ..." 
-   cmd=`sudo docker run --name jenkins-blueocean --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   --volume "$HOME":/home   --restart=on-failure   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true"   myjenkins-blueocean:$VERSION`
+   sudo docker run --name jenkins-blueocean --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   --volume "$HOME":/home   --restart=on-failure   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" myjenkins-blueocean:$VERSION
 
-   cmd=`sudo docker ps | grep jenkins-blueocean`
+   sudo docker ps | grep jenkins-blueocean
    if [ $? -eq 0 ]
    then
       echo "Jenkins ocean OK"

fc660e00ee4219732af8b5bcff237de0d23974c5
toto <toto>
2022-11-21 08:18:07 +0100

    evol

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Pour lancer ce script  :
+# Pour lancer ce script 
 # 1 rendre le script est executable (a faire une seule fois) : chmod u+x restart.sh
 # 2 executer le script : ./restart.sh
 

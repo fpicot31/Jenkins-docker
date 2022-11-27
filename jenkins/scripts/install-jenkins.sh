aefb078aa11e55473da4e190dbb4b10c5766db4d
francois <francois.picot@atos.net>
2022-11-27 12:08:05 +0100

    reorganisation

--- jenkins/scripts/install-jenkins.sh
+++ jenkins/scripts/install-jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Adding apt-keys"
+echo "Installing Jenkins ..."
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 
--- jenkins/scripts/stop.sh
+++ jenkins/scripts/stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-# Pour lancer ce script
+# Pour lancer ce script:
 # 1 rendre le script est executable (a faire une seule fois) : chmod u+x stop.sh
 # 2 executer le script : ./stop.sh
 
-cmd=`docker stop $(docker ps -aq) > /dev/null 2>&1`
-cmd=`docker rm $(docker ps -aq) > /dev/null 2>&1`
-cmd=`docker ps -aq`
+sudo docker stop $(sudo docker ps -aq) > /dev/null 2>&1
+sudo docker rm $(sudo docker ps -aq) > /dev/null 2>&1
 
+cmd=`sudo docker ps -aq`
 if [ -z "$cmd" ]
 then
    echo "Plus de containers actifs et inactifs"
